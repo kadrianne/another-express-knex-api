33e0b80f7114f04ed8caa1f0990517db5fa664db
kadrianne <kristine.a.du@gmail.com>
2020-05-01 11:41:54 -0600

    Add cors.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const knex = require('knex') // Package also file ./node_modules/knex/index.js
 const config = require('./knexfile')[process.env.NODE_ENV || 'development']
 const database = knex(config)
 
+app.use(cors())
 
 app.get("/dogs", (request,response) => {
     database('dogs').select()
